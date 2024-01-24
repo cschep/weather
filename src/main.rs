2df9ff141056bd44a676b56675f720b4a9ec10f0
Chris Schepman <chris@schepman.org>
2024-01-24 14:07:50 -0500

    ureq version

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 use std::error::Error;
+use std::sync::Arc;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let body = reqwest::blocking::get("http://wttr.in/11231?format=%c%t")?.text()?;
+    // let agent = ureq::AgentBuilder::new().build();
+    let agent = ureq::AgentBuilder::new()
+        .tls_connector(Arc::new(native_tls::TlsConnector::new()?))
+        .build();
+    let body = agent
+        .get("http://wttr.in/11231?format=%c%t")
+        .call()?
+        .into_string()?;
+
     println!("{}", body.replace(" ", ""));
+
     Ok(())
 }
