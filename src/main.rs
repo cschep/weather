7d00a17323804855e8de6598a0fde8056ea031c8
Chris Schepman <chris@schepman.org>
2024-08-17 10:36:57 -0700

    might as well save a redirect

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let body = reqwest::blocking::get("http://wttr.in/99202?format=%c%t")?.text()?;
+    let body = reqwest::blocking::get("https://wttr.in/99202?format=%c%t")?.text()?;
     println!("{}", body.replace(" ", ""));
     Ok(())
 }
