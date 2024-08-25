7fcd3305552a829eccfab1d4099420df9002d13a
Chris Schepman <chris@schepman.org>
2024-08-24 23:00:04 -0700

    polling and writing to the weather file directly!

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,35 @@
-use std::error::Error;
+use core::time;
+use std::fs::File;
+use std::io::Write;
+use std::{error::Error, thread};
+
+fn fetch_weather() -> Result<String, Box<dyn Error>> {
+    let body = reqwest::blocking::get("https://wttr.in/99202?format=%c%t")?.text()?;
+    let weather = body.replace(" ", "");
+    Ok(weather)
+}
+
+fn save(s: String) {
+    let mut file = File::options()
+        .write(true)
+        .truncate(true)
+        .open("/home/cschep/weather.txt")
+        .expect("can't open weather.txt");
+
+    file.write_all(s.as_bytes())
+        .expect("unable to write weather.txt");
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let body = reqwest::blocking::get("https://wttr.in/99202?format=%c%t")?.text()?;
-    println!("{}", body.replace(" ", ""));
-    Ok(())
+    let seconds = 5;
+    let five_seconds = time::Duration::from_secs(seconds);
+    println!("weather is online. polling every {} seconds", seconds);
+
+    loop {
+        let weather = fetch_weather().expect("weather fetching failed!");
+        println!("{}", weather);
+        save(weather);
+        thread::sleep(five_seconds);
+    }
+    // Ok(())
 }
